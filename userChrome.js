97c72f600502543e9eb6b534234b9c9fce86000e
thepante <panterua@hotmail.com>
2019-02-04 17:00:38 -0300

    improved code

--- userChrome.js
+++ userChrome.js
@@ -5,21 +5,21 @@
 // @note           u/thepante
 // ==/UserScript==
 
-document.getElementById("sidebar").src='chrome://browser/content/webext-panels.xul';
-document.getElementById("sidebar-button").checked=false;
 var alfs = document.getElementById("sidebar-box");
+var sideB = document.getElementById("sidebar-button");
 var ogclass = alfs.className;
 var statbnt = 0;
 
 alfs.hidden=false;
 alfs.checked=true;
 alfs.className = ogclass + ' closeit';
+sideB.checked=false;
 
 document.getElementById("sidebar-button").addEventListener('click', function(e){
 	event.preventDefault();
 	if (statbnt == 0) {
     	console.log(statbnt+" : alfs open");
-        document.getElementById("sidebar-button").checked=true;
+        sideB.checked=true;
         alfs.className = ogclass + ' openit';
         alfs.hidden=false;
         statbnt = 1;
@@ -27,7 +27,7 @@ document.getElementById("sidebar-button").addEventListener('click', function(e){
       
     else {
     	console.log(statbnt+" : alfs close");
-        document.getElementById("sidebar-button").checked=false;
+        sideB.checked=false;
         alfs.className = ogclass + ' closeit';
         alfs.hidden=false;
         statbnt = 0;
