0adbe4d12292d3c4c604caf5da4226de44c31628
thepante <panterua@hotmail.com>
2019-02-14 12:39:44 -0300

    correct

--- alfs.prefs.uc.js
+++ alfs.prefs.uc.js
@@ -6,5 +6,5 @@ var alfsPrefs={
   'keybind_ctrl'      : 1, // 1 = 'ctrl', 2 = 'alt', 0 = sortcut disabled
   'keybind_key'       : 88, // any valid - visit https://keycode.info/ for values
   'debug'             : false, // set to 'true' to watch it working on console / ctrl+shift+j
-  'classic_mode'      : true, // do not float. Firefox default behavior + keep content loaded
+  'classic_mode'      : false, // do not float. Firefox default behavior + keep content loaded
 };
